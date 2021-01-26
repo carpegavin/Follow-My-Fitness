1f6f0abc4cbf17631bd72445004287acac91af00
Jared Ackley <jrod3323@gmail.com>
2021-01-25 19:39:59 -0600

    set up iniital connection for sequelize

---
+++ routes/api-routes.js
@@ -0,0 +1,16 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+// Requiring our Todo model
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+ 
+};
