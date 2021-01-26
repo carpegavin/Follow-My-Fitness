1285f1d313dde69b68000bd8ef492c43cad57354
Jared Ackley <jrod3323@gmail.com>
2021-01-25 20:21:40 -0600

    add workout model

---
+++ models/workouts.js
@@ -0,0 +1,45 @@
+module.exports = function(sequelize, DataTypes) {
+    let Workout = sequelize.define("Workout", {
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+              isEmail: true
+            }
+          },
+        workout_type: {
+              type: DataTypes.STRING,
+              allowNull: false
+          },
+        sets: {
+              type: DataTypes.INTEGER,
+              allowNull:true
+          },
+        reps: {
+            type: DataTypes.INTEGER,
+            allowNull:true
+        },
+        distance: {
+            type: DataTypes.INTEGER,
+            allowNull:true
+        },
+        date: {
+            type: DataTypes.DATEONLY ,
+            allowNull: false
+        },
+        weight_lifted: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        exercise_name: {
+            type: DataTypes.STRING,
+            allowNull:false
+        },
+        time_workout: {
+            type: DataTypes.INTEGER,
+            allowNull:true
+        }
+    });
+    return Workout;
+  };
