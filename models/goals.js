bb8e99dbdb6f7817fc89cb02c7600372eaf30c46
Devante W <williams.devante05@gmail.com>
2021-01-25 19:56:31 -0600

    fix extra character in variable

--- models/goals.js
+++ models/goals.js
@@ -1,5 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
-    var Goals = sequelize.define("Goal", {
+    let Goal = sequelize.define("Goal", {
       goalSetByUser: {
         type: DataTypes.STRING,
         allowNull: false,
