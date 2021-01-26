394c35a94495550878b5d429ac1d5021a1de4052
Issac Loya <misterloya@gmail.com>
2021-01-25 19:45:30 -0600

    testing branch

--- server.js
+++ server.js
@@ -18,3 +18,14 @@ var routes = require("./controllers/fitness_controller.js");
 app.listen(PORT, function() {
   console.log("Server listening on: http://localhost:" + PORT);
 });
+
+// BMI CALCULATOR
+// // weight is in pounds
+// // height will need to be converted to inches
+
+function calculator(weight, height) {
+  var results = weight / height / height * 703; 
+  console.log(results);
+}
+
+calculator(300,72);
