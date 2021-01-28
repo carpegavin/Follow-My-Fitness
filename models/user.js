b290e26fbd93aa9bbbb2b46ffacd13fa2294e700
Jared Ackley <jrod3323@gmail.com>
2021-01-27 21:18:53 -0600

    at createdAt to all models

--- models/user.js
+++ models/user.js
@@ -28,6 +28,11 @@ module.exports = function(sequelize, DataTypes) {
     height: {
         type: DataTypes.INTEGER,
         allowNull: false
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
     }
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
