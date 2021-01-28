6da62d3d94d3be57cda8292ccc99665b8c3d2306
Jared Ackley <jrod3323@gmail.com>
2021-01-27 18:40:18 -0600

    add jaws variables to config

--- config/config.js
+++ config/config.js
@@ -16,11 +16,7 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASS,
-    "database": "fitness",
-    "host": "127.0.0.1",
-    "port": 3306,
+    "use_env_variable": "JAWSDB_URL",
     "dialect": "mysql"
   }
 }
